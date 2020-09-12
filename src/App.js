3577fe8a2f0efade348c03f487dd2495ef9310d7
Alonzo Thomas <althomas@bills.com>
2020-09-12 16:32:54 -0700

    Load one feed at a time

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import * as R from "ramda";
+import {
+	Group,
+	Image,
+	Box,
+	Set,
+	Stack,
+	Heading,
+	Paragraph,
+	Link,
+	Button,
+	InputField,
+	Spinner,
+	Container,
+} from "bumbag";
+import { Formik, Form, Field } from "formik";
+import { useQuery } from "draqula";
+import gql from "graphql-tag";
+
+const FEED_QUERY = gql`
+	query Feed($url: String!) {
+		feed(url: $url) {
+			id
+			title
+			items {
+				id
+				title
+				image
+				link
+			}
+		}
+	}
+`;
+
+function LinkCard({ title, link, channel, image }) {
+	return (
+		<Link href={link} _hover={{ textDecoration: "none" }} fontWeight="400">
+			<Group
+				backgroundColor="background"
+				altitude="300"
+				borderRadius="4"
+				orientation="vertical"
+				width="300px"
+			>
+				<Image width="300px" height="225px" src={image} />
+				<Stack spacing="major-1" padding="major-2">
+					<Heading use="h6" marginBottom="minor-">
+						{title}
+					</Heading>
+					<Paragraph color="text100" fontSize="100">
+						{channel}
+					</Paragraph>
+				</Stack>
+			</Group>
+		</Link>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [url, setUrl] = useState(
+		"https://www.youtube.com/feeds/videos.xml?channel_id=UCIyIoM_Nd8HtY19fuR_ov2A"
+	);
+	const { data, error, isLoading } = useQuery(FEED_QUERY, {
+		url,
+	});
+
+	return (
+		<Container padding="major-4">
+			{error && <span>Error: {error.message}</span>}
+			<Stack>
+				<Formik initialValues={{ url }} onSubmit={(data) => setUrl(data.url)}>
+					<Form>
+						<Stack orientation="horizontal">
+							<Field component={InputField.Formik} name="url" />
+							<Button palette="primary" type="submit">
+								Get Feed
+							</Button>
+						</Stack>
+					</Form>
+				</Formik>
+				{isLoading && <Spinner size="large" />}
+				{data && (
+					<Set spacing="major-4">
+						{data.feed.items.map((item) => (
+							<LinkCard
+								key={item.id}
+								title={item.title}
+								link={item.link}
+								channel={data.feed.title}
+								image={item.image}
+							/>
+						))}
+					</Set>
+				)}
+			</Stack>
+		</Container>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider as BumbagProvider } from "bumbag";
+import { Draqula, DraqulaProvider } from "draqula";
+
+const client = new Draqula(
+	"https://gql-rss-parser.lenell16.vercel.app/api/graphql"
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<BumbagProvider>
+			<DraqulaProvider client={client}>
+				<App />
+			</DraqulaProvider>
+		</BumbagProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
